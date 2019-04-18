3226b96e5624f6fbe8db666174785540ff19a707
Sudhil Gauchan <s14145@gmail.com>
2019-04-17 21:48:57 -0500

    Added More TextBox Validation

--- src/com/conversion/ConversionMain.java
+++ src/com/conversion/ConversionMain.java
@@ -98,7 +98,7 @@ public class ConversionMain {
 			@Override
 			public void keyTyped(KeyEvent e) {
 				char c=e.getKeyChar();
-				if(!(Character.isDigit(c) || (c==KeyEvent.VK_BACK_SPACE)|| c==KeyEvent.VK_DELETE)) {
+				if(!(Character.isDigit(c) || (c==KeyEvent.VK_BACK_SPACE)|| c==KeyEvent.VK_DELETE || textField.getText().contains("-")) && textField.getText().indexOf('.') != -1) {
 					show_validation_here_1.setText("Invalid input. Number Only");
 				}else if(textField.getText().isEmpty()) {
 					show_validation_here_1.setText("");
@@ -147,7 +147,7 @@ public class ConversionMain {
 			@Override
 			public void keyTyped(KeyEvent e) {
 				char c=e.getKeyChar();
-				if(!(Character.isDigit(c) || (c==KeyEvent.VK_BACK_SPACE)|| c==KeyEvent.VK_DELETE)) {
+				if(!(Character.isDigit(c) || (c==KeyEvent.VK_BACK_SPACE)|| c==KeyEvent.VK_DELETE || textField_1.getText().contains("-")) && textField_1.getText().indexOf('.') != -1) {
 					show_validation_here_2.setText("Invalid input. Number Only");
 				}else if(textField.getText().isEmpty()) {
 					show_validation_here_2.setText("");
